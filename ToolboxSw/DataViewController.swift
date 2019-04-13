731b7318a41d805339e0ab598301f2b121626ad6
loveuncg <loveun1988@outlook.com>
2019-04-13 12:42:37 -0500

    Initial Commit

---
+++ ToolboxSw/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  ToolboxSw
+//
+//  Created by loveun Cg on 4/13/19.
+//  Copyright © 2019 loveun Cg. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
